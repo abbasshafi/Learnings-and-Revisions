364d3a2e09aa5948d82f0185264cdd67029a2f55
abbasshafi <abbasshafi.2000@gmail.com>
2024-01-19 12:12:10 +0500

    updated files in Langchain/K folder

--- Artificial Intelligence/GenAI/LangChain/Krish/main.py
+++ Artificial Intelligence/GenAI/LangChain/Krish/main.py
@@ -10,7 +10,8 @@ st.title("Langchain Demo with OpenAI")
 input_text=st.text_input("Search the topic u want")
 
 ##OpenAI LLMs
-llm=OpenAI(temperature=0.8)
+llm = OpenAI(temperature=0.8, openai_api_key="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")
+# llm=OpenAI(temperature=0.8)
 
 if input_text:
     st.write(llm(input_text))
