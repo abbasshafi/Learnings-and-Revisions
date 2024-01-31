922e3c5cfaf967b34d05e572daf26b169898bc44
abbasshafi <abbasshafi.2000@gmail.com>
2024-01-31 17:11:10 +0500

    added and updated files

--- Artificial Intelligence/GenAI/LangChain/CodeBasic/main.py
+++ Artificial Intelligence/GenAI/LangChain/CodeBasic/main.py
@@ -10,3 +10,7 @@ from langchain_community.document_loaders.csv_loader import CSVLoader
 loader= CSVLoader('movies.csv',source_column='title')
 data=loader.load()
 len(data)
+
+data[0].metadata
+
+from langchain_community.document_loaders import UnstructuredURLLoader
