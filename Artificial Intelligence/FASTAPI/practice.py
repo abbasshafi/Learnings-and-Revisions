ea61f7916d81c7a3e61b164767f6e63f1f913c9a
abbasshafi <abbasshafi.2000@gmail.com>
2024-03-12 15:56:15 +0500

    updated FastAPI/practice.py

--- Artificial Intelligence/FASTAPI/practice.py
+++ Artificial Intelligence/FASTAPI/practice.py
@@ -3,9 +3,9 @@ from pydantic import BaseModel
 
 app = FastAPI()
 
-class Hello(BaseModel):
-    name: str
-    age: int
+# class Hello(BaseModel):
+#     name: str
+#     age: int
 
 @app.get('/')
 async def msg():
@@ -13,4 +13,13 @@ async def msg():
 
 @app.get('/{name}')
 async def msg(name:str):
-    return f"Hello Muhtaram {name}!"
+    if name == "abbas":
+        return "aji senga chal de"
+    elif name == "Majid":
+        return "qarayyy senga chal de"
+    elif name == "Nomi":
+        return "da Engineer zuma senga chal de"
+    
+    else:
+        return "Hello mate"
+    
